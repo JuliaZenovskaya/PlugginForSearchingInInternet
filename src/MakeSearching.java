5a5fee9473e734907a6d6f575b8f477ea55302ea
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-03-21 05:30:55 +0300

    first part of work done

---
+++ src/MakeSearching.java
@@ -0,0 +1,24 @@
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.project.Project;
+
+import java.util.Objects;
+
+public class MakeSearching extends AnAction {
+
+    public String choice;
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        CaretModel caretModel = Objects.requireNonNull(e.getData(LangDataKeys.EDITOR)).getCaretModel();
+        Caret caret = caretModel.getCurrentCaret();
+        String textForSearching = caret.getSelectedText();
+
+        BrowserUtil.browse(choice + textForSearching);
+    }
+}
--- src/SearchingPlaceChoosing.java
+++ src/SearchingPlaceChoosing.java
@@ -1,11 +1,47 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SearchingPlaceChoosing {
+    private MakeSearching makeSearching;
+
     private JRadioButton GitHub;
     private JRadioButton Habr;
     private JRadioButton StackOverflow;
-    private JPanel Choce;
+    private JPanel Choice;
     private JRadioButton Toster;
 
-    
+    public SearchingPlaceChoosing (MakeSearching makeSearching){
+        this.makeSearching = makeSearching;
+
+        GitHub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                makeSearching.choice = "https://github.com/search?q==";
+            }
+        });
+
+        Habr.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                makeSearching.choice = "https://habr.com/ru/search/?q=";
+            }
+        });
+
+        StackOverflow.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                makeSearching.choice = "https://ru.stackoverflow.com/search?q=";
+            }
+        });
+
+        Toster.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                makeSearching.choice = "https://toster.ru/search?q=";
+            }
+        });
+    }
+
 }
