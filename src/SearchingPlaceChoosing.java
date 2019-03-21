5a1475101c0d7c716ed4f8ee693830b2b0977f86
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-03-21 05:01:28 +0300

    Initial commit

---
+++ src/SearchingPlaceChoosing.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class SearchingPlaceChoosing {
+    private JRadioButton GitHub;
+    private JRadioButton Habr;
+    private JRadioButton StackOverflow;
+    private JPanel Choce;
+}
